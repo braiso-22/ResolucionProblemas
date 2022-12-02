fc2d16077bb138e2f4ea6fa1676eef20f9e18010
Brais <braisfv22@gmail.com>
2022-12-02 18:10:24 +0100

    mejorada la reproducción

--- ndamas_genetico.py
+++ ndamas_genetico.py
@@ -1,5 +1,7 @@
 import random
 
+def genera_random(numero):
+    return random.randint(0, numero-1)
 
 class Estado:
     def __init__(self, posiciones):
@@ -13,13 +15,22 @@ class Estado:
         # creamos una lista de posiciones vacía
         posiciones = []
         # bucle para ir cogiendo posiciones de los padres
+        
+        num = genera_random(len(self.posiciones))
+
+        better = self if self.mal_colocadas < otro.mal_colocadas else otro
+
+        worse = self if self.mal_colocadas > otro.mal_colocadas else otro
+
         for i in range(len(self.posiciones)):
-            total = self.mal_colocadas + otro.mal_colocadas
-            # ejemplo 5 y 1
-            if random.randint(0, total) < self.mal_colocadas:
-                posiciones.append(self.posiciones[i])
-            else:
-                posiciones.append(otro.posiciones[i])
+            if i == num:
+                posiciones.append(
+                    worse.posiciones[i]
+                )
+                continue
+            posiciones.append(better.posiciones[i])
+
+        posiciones[genera_random(len(self.posiciones))] = Posicion().get_random_posicion(len(self.posiciones))
         # devolvemos el nuevo individuo
         return Estado(posiciones)
 
@@ -30,23 +41,37 @@ class Estado:
         0 es lo mejor, y ndamas lo peor
         '''
         mal_colocadas = 0
+        # Están en la misma casilla
+        for i, dama in enumerate(self.posiciones):
+            for j, dama2 in enumerate(self.posiciones):
+                if i == j:
+                    continue
+                if dama.fila == dama2.fila and dama.columna == dama2.columna:
+                    mal_colocadas += 1
+
         # bucle para contar las que están en la misma fila,
         # incrementando mal_colocadas por cada una que esté mal
-        for dama in self.posiciones:
-            for dama2 in self.posiciones:
+        for i, dama in enumerate(self.posiciones):
+            for j, dama2 in enumerate(self.posiciones):
+                if i == j:
+                    continue
                 if dama.fila == dama2.fila and dama.columna != dama2.columna:
                     mal_colocadas += 1
 
         # bucle para contar las que están en la misma columna,
         # incrementando mal_colocadas por cada una que esté mal
-        for dama in self.posiciones:
-            for dama2 in self.posiciones:
+        for i, dama in enumerate(self.posiciones):
+            for j, dama2 in enumerate(self.posiciones):
+                if i == j:
+                    continue
                 if dama.columna == dama2.columna and dama.fila != dama2.fila:
                     mal_colocadas += 1
         # bucle para contar las que están en la misma diagonal,
         # incrementando mal_colocadas por cada una que esté mal
-        for dama in self.posiciones:
-            for dama2 in self.posiciones:
+        for i, dama in enumerate(self.posiciones):
+            for j, dama2 in enumerate(self.posiciones):
+                if i == j:
+                    continue
                 if abs(dama.fila - dama2.fila) == abs(dama.columna - dama2.columna) and dama.fila != dama2.fila:
                     mal_colocadas += 1
 
@@ -62,14 +87,35 @@ class Estado:
     def __lt__(self, other):
         return self.mal_colocadas < other.mal_colocadas
 
+    def print(self):
+        estado = []
+        for i in range(len(self.posiciones)):
+            estado.append([])
+            for j in range(len(self.posiciones)):
+                estado[i].append("0")
+        for dama in self.posiciones:
+            estado[dama.fila][dama.columna] = "X"
+        for fila in estado:
+            string = ""
+            for columna in fila:
+                string += columna + " "
+            print(string)
+        print("\n")
+
 
 class Posicion:
-    def __init__(self, fila, columna):
+    def __init__(self, fila=0, columna=0):
         self.fila = fila
         self.columna = columna
 
     def __str__(self):
         return "Fila: "+str(self.fila)+" Columna: "+str(self.columna)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_random_posicion(self, n):
+        return Posicion(random.randint(0, n-1), random.randint(0, n-1))
 
 
 class GestorEstados:
@@ -104,38 +150,62 @@ class GestorEstados:
         # cruza los estados para generar nuevos estados
         # devuelve una lista con los nuevos estados
         nuevos_estados = []
-        for i in range(0, len(estados), 2):
+        for i in range(0, int(len(estados)/ 2)):
             # cruzamos los estados i e i+1
-            nuevo_estado = estados[i].reproducir_con(estados[i+1])
+            reproductor = len(estados)-i-1
+            nuevo_estado = estados[i].reproducir_con(estados[reproductor])
             nuevos_estados.append(nuevo_estado)
         return nuevos_estados
 
-def evolucionar(poblacion, numero_generaciones):
-    gestorEstados = GestorEstados()
-    # bucle para las generaciones
-    for i in range(numero_generaciones):
-        # evaluamos la población
-        poblacion = gestorEstados.ordena_estados(poblacion)
-        # eliminamos los peores
-        poblacion = gestorEstados.eliminar_peores(poblacion, 100)
-        # cruzamos los estados
-        poblacion_de_hijos = gestorEstados.cruza_estados(poblacion)
-        # añadimos los hijos a la población
-        poblacion = poblacion+poblacion_de_hijos
-        poblacion = gestorEstados.ordena_estados(poblacion)
 
+def evolucionar(poblacion, generaciones=0):
+    gestorEstados = GestorEstados()
+    generaciones_actuales=0
+    if generaciones == 0:
+        # bucle para las generaciones
+        while (True):
+            poblacion = gestorEstados.ordena_estados(poblacion)
+            # eliminamos los peores
+            poblacion = gestorEstados.eliminar_peores(poblacion, 100)
+            # cruzamos los estados
+            poblacion_de_hijos = gestorEstados.cruza_estados(poblacion)
+            # añadimos los hijos a la población
+            poblacion = poblacion+poblacion_de_hijos
+            generaciones_actuales+=1
+            poblacion = gestorEstados.ordena_estados(poblacion)
+            if generaciones_actuales%100==0:
+                print("Generación: "+str(generaciones_actuales))
+            if poblacion[0].mal_colocadas == 0:
+                break
+    else:
+        # bucle para las generaciones
+        for i in range(generaciones):
+            # evaluamos la población
+            poblacion = gestorEstados.ordena_estados(poblacion)
+            # eliminamos los peores
+            poblacion = gestorEstados.eliminar_peores(poblacion, 100)
+            # cruzamos los estados
+            poblacion_de_hijos = gestorEstados.cruza_estados(poblacion)
+            # añadimos los hijos a la población
+            poblacion = poblacion+poblacion_de_hijos
+            poblacion = gestorEstados.ordena_estados(poblacion)
+    print("Generaciones: "+str(generaciones_actuales))
     return poblacion
 
-def main():
-    
-    gestorEstados = GestorEstados()
 
-    poblacion = evolucionar(gestorEstados.genera_estados(8, 100), 7)
+def main():
+
+    gestorEstados = GestorEstados()
+    poblacion = evolucionar(
+        gestorEstados.genera_estados(12, 100)
+    )
 
     # evaluamos la población
-    for i,estado in enumerate(poblacion):
+    for i, estado in enumerate(poblacion):
+        estado.print()
         print(estado, "\n")
-        if(i==3):
+        
+        if (i == 1):
             break
     pass
 
