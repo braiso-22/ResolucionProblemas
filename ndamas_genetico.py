c6fcfa8f3af21f46c99905c2ba8a6b4399b538df
Brais <braisfv22@gmail.com>
2022-12-07 21:58:28 +0100

    Add matplotlib for stadistics
    Se usa matplotlib para hacer las medias de cuantas generaciones les lleva teniendo en cuenta el numero de plobacion

--- ndamas_genetico.py
+++ ndamas_genetico.py
@@ -1,7 +1,10 @@
 import random
+import matplotlib.pyplot as plt
+
 
 def genera_random(numero):
     return random.randint(0, numero-1)
+
 
 class Estado:
     def __init__(self, posiciones):
@@ -15,7 +18,7 @@ class Estado:
         # creamos una lista de posiciones vacía
         posiciones = []
         # bucle para ir cogiendo posiciones de los padres
-        
+
         num = genera_random(len(self.posiciones))
 
         better = self if self.mal_colocadas < otro.mal_colocadas else otro
@@ -30,7 +33,8 @@ class Estado:
                 continue
             posiciones.append(better.posiciones[i])
 
-        posiciones[genera_random(len(self.posiciones))] = Posicion().get_random_posicion(len(self.posiciones))
+        posiciones[genera_random(len(self.posiciones))] = Posicion(
+        ).get_random_posicion(len(self.posiciones))
         # devolvemos el nuevo individuo
         return Estado(posiciones)
 
@@ -150,7 +154,7 @@ class GestorEstados:
         # cruza los estados para generar nuevos estados
         # devuelve una lista con los nuevos estados
         nuevos_estados = []
-        for i in range(0, int(len(estados)/ 2)):
+        for i in range(0, int(len(estados) / 2)):
             # cruzamos los estados i e i+1
             reproductor = len(estados)-i-1
             nuevo_estado = estados[i].reproducir_con(estados[reproductor])
@@ -158,24 +162,26 @@ class GestorEstados:
         return nuevos_estados
 
 
-def evolucionar(poblacion, generaciones=0):
+def evolucionar(poblacion, sobrevivientes=10, generaciones=0, limite=2000):
     gestorEstados = GestorEstados()
-    generaciones_actuales=0
+    generaciones_actuales = 0
     if generaciones == 0:
         # bucle para las generaciones
         while (True):
             poblacion = gestorEstados.ordena_estados(poblacion)
             # eliminamos los peores
-            poblacion = gestorEstados.eliminar_peores(poblacion, 100)
+            poblacion = gestorEstados.eliminar_peores(
+                poblacion, sobrevivientes)
             # cruzamos los estados
             poblacion_de_hijos = gestorEstados.cruza_estados(poblacion)
             # añadimos los hijos a la población
             poblacion = poblacion+poblacion_de_hijos
-            generaciones_actuales+=1
+            generaciones_actuales += 1
             poblacion = gestorEstados.ordena_estados(poblacion)
-            if generaciones_actuales%100==0:
+            if generaciones_actuales % 500 == 0:
                 print("Generación: "+str(generaciones_actuales))
-            if poblacion[0].mal_colocadas == 0:
+
+            if poblacion[0].mal_colocadas == 0 or generaciones_actuales >= limite:
                 break
     else:
         # bucle para las generaciones
@@ -183,31 +189,102 @@ def evolucionar(poblacion, generaciones=0):
             # evaluamos la población
             poblacion = gestorEstados.ordena_estados(poblacion)
             # eliminamos los peores
-            poblacion = gestorEstados.eliminar_peores(poblacion, 100)
+            poblacion = gestorEstados.eliminar_peores(
+                poblacion, sobrevivientes)
             # cruzamos los estados
             poblacion_de_hijos = gestorEstados.cruza_estados(poblacion)
             # añadimos los hijos a la población
             poblacion = poblacion+poblacion_de_hijos
             poblacion = gestorEstados.ordena_estados(poblacion)
     print("Generaciones: "+str(generaciones_actuales))
-    return poblacion
+    return poblacion, generaciones_actuales
 
 
-def main():
+def menu():
+    print("1. Generar poblaciones")
+    print("2. Mostrar primero en cada población")
+    print("3. Evolucionar")
+    print("4. Mostrar estadisticas")
+    print("0. Salir")
+    try:
+        opcion = int(input("Introduce una opción: "))
+    except:
+        opcion = 0
+    return opcion
 
-    gestorEstados = GestorEstados()
-    poblacion = evolucionar(
-        gestorEstados.genera_estados(12, 100)
-    )
 
-    # evaluamos la población
+def mostrar_poblacion(poblacion, limite=5):
     for i, estado in enumerate(poblacion):
         estado.print()
         print(estado, "\n")
-        
-        if (i == 1):
+        if (i == limite):
             break
-    pass
+
+
+def generar_poblaciones(numero_poblaciones):
+    gestorEstados = GestorEstados()
+    poblaciones = {}
+    for i in range(numero_poblaciones):
+        n = int(input(
+            f"Escribe el numero de pobladores en la población numero {i+1}: "))
+        poblaciones[str(n)] = [gestorEstados.genera_estados(8, n), 0]
+    return poblaciones
+
+
+def generar_poblaciones_de_existentes(poblaciones):
+    gestorEstados = GestorEstados()
+    for key in poblaciones.keys():
+        poblaciones[key] = [gestorEstados.genera_estados(8, int(key)), 0]
+    return poblaciones
+
+
+def evolucionar_poblaciones(poblaciones):
+    for key in poblaciones.keys():
+        poblaciones[key][0], poblaciones[key][1] = evolucionar(
+            poblaciones[key][0], sobrevivientes=int(key))
+
+
+def genera_media(poblaciones, num):
+    media = {}
+    for key in poblaciones.keys():
+        media[key] = 0
+    for i in range(num):
+        evolucionar_poblaciones(poblaciones)
+        for key in poblaciones.keys():
+            media[key] += poblaciones[key][1]
+        if i!=num-1:
+            poblaciones = generar_poblaciones_de_existentes(poblaciones)
+    for key in poblaciones.keys():
+        media[key] /= num
+
+    return media
+
+
+def main():
+    poblaciones = {}
+    media = {}
+    while (True):
+        opcion = menu()
+        if opcion == 1:
+            num_poblaciones = int(
+                input("Cuantas poblaciones quieres generar?"))
+            poblaciones = generar_poblaciones(int(num_poblaciones))
+        elif opcion == 2:
+            for poblacion_actual in poblaciones.values():
+                mostrar_poblacion(poblacion_actual[0], limite=0)
+        elif opcion == 3:
+            num = int(
+                input("Introduce el numero de veces que quieres que se ejecute: "))
+            media = genera_media(poblaciones, num)
+
+        elif opcion == 4:
+            # estadisticas
+            plt.bar(media.keys(), media.values())
+            plt.show()
+        elif opcion == 0:
+            break
+        else:
+            print("Opción incorrecta")
 
 
 if __name__ == '__main__':
